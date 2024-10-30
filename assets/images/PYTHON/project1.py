336dfc8da819fdf758f9d3406822f4740db458c0
Esther Kariuki <esywacu@gmail.com>
2024-10-30 04:19:59 +0000

    Added images to the gallery

---
+++ assets/images/PYTHON/project1.py
@@ -0,0 +1,7 @@
+name = input("Hey type your name")
+print("Hello", name, "welcome to my game1")
+
+should_we_play = input("Do you want to play?").lower
+
+if should_we_play == "yes":
+    print("we are gonna play!")
